40eb321ffc498e391c3bce43332be68f88144e53
shoutaoumaru <otokmae.black@gmail.com>
2021-09-11 09:44:57 +0900

    first commit

---
+++ scripts/libs/hero-slider.js
@@ -0,0 +1,31 @@
+class HeroSlider {
+  constructor(el) {
+    this.el = el;
+    this.swiper = this._initSwiper();
+  }
+
+  _initSwiper() {
+    return new Swiper(this.el, {
+      loop: true,
+      grabCursor: false,
+      effect: 'fade',
+      slidesPerView: 1,
+      speed: 3000,
+    });
+  }
+
+  start(options = {}) {
+    options = Object.assign({
+        delay: 6000,
+        disableOnInteraction: false,
+      },
+      options
+    );
+
+    this.swiper.params.autoplay = options;
+    this.swiper.autoplay.start();
+  }
+  stop() {
+    this.swiper.autoplay.stop();
+  }
+}
